74cc3bd69b6e5fb5b67d02659482050a06c60950
SwadowMaster <72133740+SwadowMaster@users.noreply.github.com>
2025-10-07 17:48:08 +0200

    tests

--- client-cofig.bat
+++ client-cofig.bat
@@ -1,32 +1,154 @@
 ::Script SOR WindowServer config - Alejandro Santana, 2ºSMR
-::            FUCIONALIDADES
-::Asigna las IP estáticas de los clientes.
-::Asigna el nombre de los clientes.
-::Asigna las IP automáticas (DHCP) de los clientes.
+::          FUNCIONALIDADES
+::Asigns static IPs to the clients.
+::Asigns the name of the clients.
+::Asigns automatic IPs (DHCP) to the clients.
 @echo off
 :: Allow to use vairbles with !-!
 setlocal enabledelayedexpansion
 
-Echo Basic windows client config SOR - Alejandro Santana
-::Ask for IP address of the client
-set /p IP_CLIENT=Enter IP address of the client: 
-set /p MASK=Enter subnet mask: 
-set /p GATEWAY=Enter default gateway: 
+:MENU
+cls
+echo ========================================
+echo    Configurador basico para Clientes Windows - SOR
+echo    Hecho por Alejandro Santana - 2ºSMR
+echo ========================================
+echo.
+echo Por favor, elige una opcion:
+echo   1. Configurar la red (IP Fija o DHCP)
+echo   2. Cambiar el nombre del equipo
+echo   3. Salir
+echo.
+set /p MAIN_OPT=Que quieres hacer (1/2/3)?: 
 
-::Ask for interface name
-set /p INTERFACE_NAME=Enter name to assign as interface name: 
+if "%MAIN_OPT%"=="1" goto CONFIG_NET
+if "%MAIN_OPT%"=="2" goto CONFIG_HOSTNAME
+if "%MAIN_OPT%"=="3" exit /b
+echo Opcion no valida
+pause
+goto MENU
 
-::Ask for client name
-set /p NAME_CLIENT=Enter client name: 
+::-----------------------------------------------------
+:CONFIG_NET
+cls
+Echo ========================================
+Echo    Configurador de red para clientes
+Echo ========================================
 
-::Assign IP address and interface name
-netsh int ip set address name=!INTERFACE_NAME! source=static addr=!IP_CLIENT! mask=!MASK! gateway=!GATEWAY!
-
-::Assign client name
-wmic path Win32_ComputerSystem where "name='!NAME_CLIENT!'" call rename name=!NAME_CLIENT!
-for /f "tokens=2 delims=:" %%a in ('netsh int ip show config name ^!INTERFACE_NAME! ^| findstr /i "IP Address"') do (
-    if "%%a" == "IP Address" (
-        echo La IP de este equipo es %%b
+::Show network interfaces detected in the system
+echo.
+echo Buscando las tarjetas de red...
+set i=0
+for /f "skip=3 tokens=4*" %%A in ('netsh interface show interface') do (
+    if not "%%A"=="" (
+        set /a i+=1
+        set "iface[!i!]=%%A %%B"
+        echo   !i!. %%A %%B
     )
 )
+
+if %i%==0 (
+    echo No hay ninguna tarjeta de red
+    pause
+    goto MENU
+)
+
+::Ask the user to select one interface
+echo.
+set /p INTERFACE_NUM=Dime el numero de la tarjeta que quieres configurar: 
+set "INTERFACE_NAME=!iface[%INTERFACE_NUM%]!"
+if "!INTERFACE_NAME!"=="" (
+    echo Ese numero no vale
+    pause
+    goto MENU
+)
+echo Has elegido: "!INTERFACE_NAME!"
+echo.
+
+::Ask for network configuration mode
+echo Como quieres configurar la red:
+echo   1. IP Fija (Estatica)
+echo   2. DHCP (Automatica)
+set /p OPTION=Elige una opcion (1/2): 
+
+if "%OPTION%"=="1" (
+    ::Static IP configuration
+    echo.
+    set /p IP_CLIENT=Mete la direccion IP del cliente: 
+    set /p MASK=Mascara de subred: 
+    set /p GATEWAY=Puerta de enlace: 
+
+    echo Vale, configurando la IP en "!INTERFACE_NAME!"...
+    netsh interface ipv4 set address name="!INTERFACE_NAME!" static !IP_CLIENT! !MASK! !GATEWAY!
+    if %errorlevel% neq 0 (
+        echo Uupsi, ha habido un error. Revisa los datos que has metido
+        pause
+        goto MENU
+    )
+) else if "%OPTION%"=="2" (
+    ::DHCP configuration
+    echo Activando DHCP para "!INTERFACE_NAME!"...
+    netsh interface ipv4 set address name="!INTERFACE_NAME!" source=dhcp
+    netsh interface ipv4 set dnsservers name="!INTERFACE_NAME!" source=dhcp
+) else (
+    echo Opcion incorrecta
+    pause
+    goto MENU
+)
+
+::Show the IP configuration
+echo.
+echo Te muestro como ha quedado la configuracion IP:
+for /f "tokens=2 delims=:" %%A in ('netsh interface ipv4 show config name^="!INTERFACE_NAME!" ^| findstr "IP Address"') do (
+    echo La direccion IP de este equipo es%%A
+)
+
+echo.
+echo Configuracion de red terminada con exito
 pause
+goto MENU
+
+::-----------------------------------------------------
+:CONFIG_HOSTNAME
+cls
+echo ========================================
+echo    Configuracion del Nombre del Equipo
+echo ========================================
+echo.
+
+set /p NAME_CLIENT=Escribe el nombre nuevo para el cliente: 
+
+:: Validate characters (avoid invalid ones)
+echo Comprobando si el nombre tiene caracteres raros...
+echo !NAME_CLIENT! | findstr /R "[\\/:*?\"^<^>^|]"
+if %errorlevel%==0 (
+    echo El nombre tiene caracteres no validos, prueba otra vez
+    pause
+    goto MENU
+)
+
+:: Assign the client name using PowerShell (modern method)
+echo Cambiando el nombre del PC a "!NAME_CLIENT!"...
+powershell.exe -ExecutionPolicy Bypass -Command "Rename-Computer -NewName '!NAME_CLIENT!' -Force | Out-Null"
+
+:: Check if the PowerShell command was successful
+if %errorlevel% equ 0 (
+    echo El cambio de nombre ha funcionado bien
+    echo OJO: El nuevo nombre se vera cuando reinicies el equipo
+    echo.
+    set /p REBOOT="Quieres reiniciar ahora para aplicar el nombre? (s/n):"
+    if /i "!REBOOT!"=="s" (
+        echo Reiniciando en 5 segundos...
+        shutdown /r /t 5 /c "Reiniciando para aplicar el nuevo nombre del equipo"
+        exit /b
+    ) else (
+        echo Acuerdate de reiniciar luego para que se cambie el nombre
+    )
+) else (
+    echo Ha fallado el cambio de nombre.
+    echo Asegurate de que estas ejecutando esto como Administrador
+    echo Puede que el nombre sea invalido o ha pasado otra cosa
+    pause
+)
+
+goto MENU
--- server-config.bat
+++ server-config.bat
@@ -1,48 +1,191 @@
-::Script SOR WindowServer config - Alejandro Santana, 2ºSMR
-::            FUCIONALIDADES
-::Asigna las IP estáticas del Servidor.
-::Asigna el nombre del Servidor.
-::Configurar servicios.
-::Activar detección de redes.
-::Instalar y configurar servicio de Enrutamiento.
-::Instalar y configurar servicio de DHCP.
-::Consultar concesión de direcciones en Servidor DHCP
+::Script SOR WindowServer config - Version Corregida y Modernizada por Gemini
+:: =====================================================================
+:: FUNCIONALIDADES:
+:: 1. Asignar IP estatica al Servidor
+:: 2. Asignar nombre al Servidor
+:: 3. Instalar roles (Enrutamiento y DHCP)
+:: 4. Configurar Firewall y Deteccion de Redes
+:: 5. Configurar el servicio DHCP (crear ambito)
+:: 6. Consultar concesiones DHCP
+:: =====================================================================
 
 @echo off
-:: Allow to use vairbles with !-!
 setlocal enabledelayedexpansion
 
-Echo Basic windows server config with DHCP SOR - Alejandro Santana
-::Request server info before set
-set /p IP_SERVER=Enter IP address of the server: 
-set /p MASK=Enter subnet mask: 
-set /p GATEWAY=Enter default gateway: 
-set /p INTERFACE_NAME=Name to assign as interfacename: 
-netsh int ip set address name=!INTERFACE_NAME! source=static addr=!IP_SERVER! mask=!MASK! gateway=!GATEWAY!
+:MENU
+cls
+echo ======================================================
+echo    Configurador Tocho de Windows Server - SOR
+echo          por Alejandro Santana
+echo ======================================================
+echo.
+echo   1. Poner IP Fija
+echo   2. Cambiar Nombre del Servidor
+echo   3. Instalar Servicios (Router y DHCP)
+echo   4. Arreglar el Firewall para ver otros PCs
+echo   5. Montar el DHCP (crear el rango de IPs)
+echo   6. Chivarme de quien tiene IP
+echo   7. Salir
+echo.
+set /p "CHOICE=Venga, dime que quieres hacer: "
 
-::Server name assignation
-::https://techcommunity.microsoft.com/t5/ask-the-directory-services-team/renaming-a-computer-using-wmic-and-how-to-get-around-that/ba-p/396043
-set /p NAME_SERVER=Enter server Name: 
-wmic path Win32_ComputerSystem where "name='!NAME_SERVER!'" call rename name=!NAME_SERVER!
-
-::Config services
-netsh firewall set service type=fileandprint opmode=enable
-netsh firewall set service type=upnp opmode=enable
-
-::To enable red detections 
-netsh advfirewall set allprofiles state on
-
-::Install and config "router services"
-netsh routing ip nat install
-netsh routing ip nat add global
-netsh routing ip nat add interface name=!INTERFACE_NAME! addr=!IP_SERVER!
-
-::Install and reuse same variables at the start
-netsh dhcp install
-set /p SCOPE_NAME=Ente name for DHCP: 
-netsh dhcp server scope add name=!SCOPE_NAME! start=!IP_SERVER! end=!IP_SERVER! mask=!MASK! state=active
-
-::Show al info 
-netsh dhcp server show scope
-
+if "%CHOICE%"=="1" goto SET_IP
+if "%CHOICE%"=="2" goto SET_HOSTNAME
+if "%CHOICE%"=="3" goto INSTALL_ROLES
+if "%CHOICE%"=="4" goto CONFIG_FIREWALL
+if "%CHOICE%"=="5" goto CONFIG_DHCP
+if "%CHOICE%"=="6" goto SHOW_LEASES
+if "%CHOICE%"=="7" exit /b
+echo Esa opcion no existe, figura.
 pause
+goto MENU
+
+:: ======================================================
+:SET_IP
+cls
+echo --- 1. CONFIGURAR IP ESTATICA ---
+echo.
+echo A ver que tarjetas de red tienes por aqui...
+set i=0
+for /f "skip=3 tokens=4*" %%A in ('netsh interface show interface') do (
+    if not "%%A"=="" (
+        set /a i+=1
+        set "iface[!i!]=%%A %%B"
+        echo   !i!. %%A %%B
+    )
+)
+if %i%==0 (
+    echo No he encontrado ninguna tarjeta de red.
+    pause
+    goto MENU
+)
+echo.
+set /p "INTERFACE_NUM=Elige el numero de la tarjeta que vas a usar: "
+set "INTERFACE_NAME=!iface[%INTERFACE_NUM%]!"
+if "!INTERFACE_NAME!"=="" (
+    echo Numero de tarjeta no valido.
+    pause
+    goto SET_IP
+)
+
+echo.
+echo Has elegido "!INTERFACE_NAME!". Ahora dame los datos:
+echo.
+set /p "IP_SERVER=IP para el servidor: "
+set /p "MASK=Mascara de subred: "
+set /p "GATEWAY=Puerta de enlace (el router): "
+set /p "DNS=Servidor DNS (puedes poner la misma IP del servidor): "
+echo.
+echo Configurando la IP en "!INTERFACE_NAME!"...
+netsh interface ipv4 set address name="!INTERFACE_NAME!" static !IP_SERVER! !MASK! !GATEWAY!
+netsh interface ipv4 set dns name="!INTERFACE_NAME!" static !DNS!
+
+if %errorlevel% neq 0 (
+    echo.
+    echo ERROR: Algo ha salido mal. Revisa los datos o si eres Administrador.
+) else (
+    echo.
+    echo ¡Listo! IP configurada.
+)
+pause
+goto MENU
+
+:: ======================================================
+:SET_HOSTNAME
+cls
+echo --- 2. CAMBIAR NOMBRE DEL SERVIDOR ---
+echo.
+set /p "NAME_SERVER=Dime el nombre nuevo para el servidor: "
+echo.
+echo Cambiando el nombre a "!NAME_SERVER!"...
+powershell.exe -ExecutionPolicy Bypass -Command "Rename-Computer -NewName '!NAME_SERVER!' -Force | Out-Null"
+
+if %errorlevel% equ 0 (
+    echo.
+    echo ¡Nombre cambiado! Pero tienes que reiniciar para que se vea el cambio.
+    set /p "REBOOT=Quieres reiniciar ahora (s/n)? "
+    if /i "!REBOOT!"=="s" (
+        shutdown /r /t 5 /c "Reinicio para aplicar el nuevo nombre del servidor."
+    )
+) else (
+    echo.
+    echo ERROR: No se pudo cambiar el nombre. ¿Lo estas ejecutando como Administrador?
+)
+pause
+goto MENU
+
+:: ======================================================
+:INSTALL_ROLES
+cls
+echo --- 3. INSTALAR SERVICIOS (ENRUTAMIENTO Y DHCP) ---
+echo.
+echo Voy a instalar DHCP y Enrutamiento. Esto puede tardar un ratillo, ten paciencia...
+powershell.exe -Command "Install-WindowsFeature DHCP, Routing -IncludeManagementTools"
+
+if %errorlevel% equ 0 (
+    echo.
+    echo ¡Servicios instalados! Ya eres un profesional.
+) else (
+    echo.
+    echo ERROR: No se han podido instalar los servicios.
+)
+pause
+goto MENU
+
+:: ======================================================
+:CONFIG_FIREWALL
+cls
+echo --- 4. ARREGLAR EL FIREWALL ---
+echo.
+echo Abriendo el firewall para que puedas ver otros equipos y compartir archivos...
+powershell.exe -Command "Enable-NetFirewallRule -DisplayGroup 'Network Discovery'"
+powershell.exe -Command "Enable-NetFirewallRule -DisplayGroup 'File and Printer Sharing'"
+
+if %errorlevel% equ 0 (
+    echo.
+    echo Firewall configurado. Ahora los PCs se deberian ver.
+) else (
+    echo.
+    echo ERROR: No se pudo configurar el firewall.
+)
+pause
+goto MENU
+
+:: ======================================================
+:CONFIG_DHCP
+cls
+echo --- 5. MONTAR EL DHCP ---
+echo OJO: Para esto, ya deberias haber puesto una IP fija.
+echo.
+echo Necesito los datos para el rango de IPs que vas a repartir:
+echo.
+set /p "SCOPE_START=IP inicial del rango: "
+set /p "SCOPE_END=IP final del rango: "
+set /p "SCOPE_MASK=Mascara de subred para el rango: "
+set /p "SCOPE_ROUTER=IP del router que le daras a los clientes: "
+set /p "SCOPE_DNS=IP del DNS que le daras a los clientes: "
+echo.
+echo Creando el ambito DHCP...
+powershell.exe -Command "Add-DhcpServerv4Scope -Name 'AmbitoSOR' -StartRange !SCOPE_START! -EndRange !SCOPE_END! -SubnetMask !SCOPE_MASK!; Set-DhcpServerv4OptionValue -DnsServer !SCOPE_DNS! -Router !SCOPE_ROUTER!"
+
+if %errorlevel% equ 0 (
+    echo.
+    echo ¡Ambito DHCP creado y funcionando! Ya puedes repartir IPs como un campeon.
+) else (
+    echo.
+    echo ERROR: No se pudo crear el ambito. ¿Instalaste el servicio DHCP antes?
+)
+pause
+goto MENU
+
+:: ======================================================
+:SHOW_LEASES
+cls
+echo --- 6. CHIVARME DE QUIEN TIENE IP ---
+echo.
+echo Estas son las IPs que ha repartido tu DHCP:
+echo.
+powershell.exe -Command "Get-DhcpServerv4Lease -ComputerName localhost -AllLeases"
+echo.
+pause
+goto MENU
